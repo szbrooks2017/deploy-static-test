4d145fa3b9c418581c69d594526f8dd1b1cfdd31
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-11 19:20:54 +0000

    adding task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#prepare servre for deployment
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+
+sudo echo "Pokemon" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sn /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubunut:ubuntu /data/
+find="^\tlocation / {"
+replace="\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\n\tlocation / {"
+sudo sed -i "s@${find}@${replace}@" /etc/nginx/sites-available/default
+
+sudo service nginx restart
+exit 0
